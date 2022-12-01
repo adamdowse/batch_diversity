b54005a932d72a509e1147f4dd6f292856e96280
Adam Dowse <adamdowse@hotmail.co.uk>
2022-12-01 14:31:51 +0000

    added run file

--- FIM_analysis.py
+++ FIM_analysis.py
@@ -5,19 +5,12 @@ import numpy as np
 
 #Collection of functions that can calculate fisher information matrix statistics
 
+def FIM_trace(fim):
+    return np.prod(fim)
 
-
-
-def calc_FIM(model,data):
-    #do some calcs
-    a = 1
-
-
-
-def FIM_trace(FIM):
-    return np.prod(FIM)
-
-
+def Get_weights(model):
+    #get the weights from a tensorflow model in a list
+    
 
 
 def FIM_diag(weights,data,total_classes,model):
@@ -26,7 +19,7 @@ def FIM_diag(weights,data,total_classes,model):
     #total_classes = the number of class outputs
     #model      = tf model that returns the logits outputs for all classes
 
-    FIM = np.zeros(len(weights))
+    fim = np.zeros(len(weights))
     data_count = 0
     for d in data:
         data_count += 1
@@ -34,7 +27,8 @@ def FIM_diag(weights,data,total_classes,model):
         output = model.predict(d) #output should be all classes
         for k in range(total_classes):
             for i in range(len(weights)):
-                FIM[i] += (weights[i]*output[k])^2
+                fim[i] += (weights[i]*output[k])^2
 
-    FIM /= data_count
+    fim /= data_count
+    return fim
 
